088ce827b70a14e1ee2b1c33a59a03543e25a2fa
codica-studio <maintenance@codicastudio.com>
2020-09-26 23:27:46 -0500

    Update

--- examples/laravel-5.1/public/index.php
+++ examples/laravel-5.1/public/index.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Michael Brown <taylorotwell@gmail.com>
+ * @author   Michael Brown <maintenance@codicastudio.com>
  */
 
 /*
